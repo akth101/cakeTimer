e804c8b8e8f99ba5e614ad3297c5b176da152f34
Myeongjin Go <mygo@cx1r2s3.42seoul.kr>
2023-09-08 17:14:11 +0900

    project init

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "caker",
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  tmpWidget(),
+                  tmpWidget(),
+                  tmpWidget(),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  tmpWidget(),
+                  tmpWidget(),
+                  tmpWidget(),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class tmpWidget extends StatelessWidget {
+  const tmpWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    MediaQueryData mediaQueryData = MediaQuery.of(context);
+    double screenWidth = mediaQueryData.size.width;
+    double screenHeight = mediaQueryData.size.height;
+    return Container(
+      width: screenWidth / 3 - screenWidth * 0.01,
+      height: screenHeight / 2 - screenWidth * 0.01,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: Colors.red,
+      ),
+      child: Image.asset('assets/images/cake1.jpg'),
+    );
+  }
+}
